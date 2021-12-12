0e45c3b9cd5112d5a9b02a6681645c70f6793465
Your Name <you@example.com>
2021-12-12 20:08:35 -0300

    Cronometro ajustado

--- cronometro.py
+++ cronometro.py
@@ -2,13 +2,16 @@ import pygame as pg
 
 pg.init()
 
+def contar_tempo():
+    tempo_atual = int(pg.time.get_ticks() / 1000)
+    tempo_restante = 60 - tempo_atual
+
+    return tempo_restante
+
 def mostrar_tempo(x, y, screen):
     fonte = pg.font.Font('assets/fontes/PressStart2P-Regular.ttf', 20)
-    tempo_atual = int(pg.time.get_ticks() / 1000)
-    
-    tempo_restante = 60 - tempo_atual
-    tempo = fonte.render(f'Tempo:{tempo_restante}', True, (255,255,255))    
+
+    tempo = fonte.render(f'Tempo:{contar_tempo()}', True, (255,255,255))    
     
     screen.blit(tempo, (x,y))
-    return tempo_restante
          
